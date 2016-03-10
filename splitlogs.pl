ae1559bc464d160a02a40c7eb6983ca94f845e0a
Christian Løverås <cl@superelectric.net>
2016-03-10 13:25:57 +0100

    Minor tweaks.

--- splitlogs.pl
+++ splitlogs.pl
@@ -9,27 +9,26 @@ use POSIX qw(strftime);
 use IO::Uncompress::Gunzip qw($GunzipError);
 
 # All logs are in the same directory: access.log.1.gz, access.log.2.gz, etc
-
 my @logs = glob "./access.log.*.gz";
-my $log = undef;
 my $verbose = 0;
-my $user = "";
-my %users = ();
+my $log = undef;
 my $zlog = undef;
+my $user = undef;
+my %users = ();
 my $today = strftime "%F", localtime;
 
 foreach $log (@logs) {
-    $zlog = IO::Uncompress::Gunzip->new($log) or die "IO::Uncompress::Gunzip failed: $GunzipError\n";
+    $zlog = IO::Uncompress::Gunzip->new($log) or die "$log: $GunzipError\n";
     while (<$zlog>) {
 	$user = (split/\s+/)[2]; # Split by whitespace, use 3rd column.
-	next if $user eq "-";
-	$users{$user} += 1;
+	next if $user eq "-"; # Skip lines without username.
+	$users{$user} += 1; #Increment for this user.
     }
 
     # Tell the human what is going on, and create separate logs.
     print "$log\n" if $verbose;
     foreach (sort keys %users) {
 	print "\t$_: $users{$_}\n" if $verbose;	
-	system("grep $_ $log > access.log.$today.$_");
+	system("grep $_ $log > access.log.$today.$_"); # Quick and dirty..
     }
 }
